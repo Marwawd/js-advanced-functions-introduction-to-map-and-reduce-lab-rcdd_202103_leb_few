4576603efe5a8848a2a9df87088e7c276b6074ab
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-28 14:46:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function mapToNegativize(array){return array.map(x => x * (-1));}
+function mapToNoChange(array){return array.map(x => x );}
+function mapToDouble(array){return array.map(x => x * 2);}
+function mapToSquare(array){return array.map(x => x*x);}
+
+let array=[1,2,3,-9];
+console.log(mapToNegativize(array));
+console.log(mapToNoChange(array));
+console.log(mapToDouble(array));
+console.log(mapToSquare(array));
+
+//const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+function reduceToTotal(sourceArray, startingPoint=0){
+  let val;
+  val=sourceArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+  return val+startingPoint;
+}
+reduceToTotal([1,2,3]);
+reduceToTotal([1,2,3],100);
+
+function reduceToAllTrue(sourceArray){
+sourceArray.reduce((a, c) =>
+{  if( typeof(c) === 'boolean' && c === true) return true;
+  else return false;}
+)};
+reduceToAllTrue([1, 2, true, "razmatazz"]);
+function reduceToAnyTrue(sourceArray){}
